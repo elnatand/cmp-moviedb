295ea0f37066fe6e7e60f9d72c1f762f71e02ff3
Elna <ederech@digibank.co.il>
2023-10-17 16:54:45 +0300

    start implement MoviesPage

--- shared/src/commonMain/kotlin/App.kt
+++ shared/src/commonMain/kotlin/App.kt
@@ -1,5 +1,7 @@
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import dev.icerock.moko.mvvm.compose.getViewModel
 import dev.icerock.moko.mvvm.compose.viewModelFactory
 
@@ -7,10 +9,13 @@ import dev.icerock.moko.mvvm.compose.viewModelFactory
 fun App() {
     MaterialTheme {
         val moviesViewModel = getViewModel(Unit, viewModelFactory { MoviesViewModel() })
-
+        MoviesPage(moviesViewModel)
     }
 }
 
-
+@Composable
+fun MoviesPage(viewModel: MoviesViewModel) {
+    val uiState by viewModel.uiState.collectAsState()
+}
 
 expect fun getPlatformName(): String
