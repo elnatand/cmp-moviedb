1ded1da53558c88ac42c9f6b2459737b45145458
Elna <ederech@digibank.co.il>
2023-11-22 18:27:57 +0200

    a

--- composeApp/src/commonMain/kotlin/com/example/moviedb/navigation/TopLevelDestination.kt
+++ composeApp/src/commonMain/kotlin/com/example/moviedb/navigation/TopLevelDestination.kt
@@ -7,7 +7,7 @@ import androidx.compose.material.icons.filled.Tv
 import androidx.compose.ui.graphics.vector.ImageVector
 import com.example.moviedb.profile.navigation.profileRoute
 import com.example.moviedb.tvshows.navigation.tvShowsRoute
-import com.example.moviedb.MR
+import com.example.moviedb.ui.MR
 import com.example.moviedb.movies.navigation.moviesRoute
 import dev.icerock.moko.resources.StringResource
 
