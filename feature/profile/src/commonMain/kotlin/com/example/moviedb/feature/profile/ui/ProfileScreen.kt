279c825b6ea5fe7d962516555d6f7f6ad8c11395
Elna <ederech@digibank.co.il>
2023-12-04 10:02:33 +0200

    set profile picture

--- feature/profile/src/commonMain/kotlin/com/example/moviedb/feature/profile/ui/ProfileScreen.kt
+++ feature/profile/src/commonMain/kotlin/com/example/moviedb/feature/profile/ui/ProfileScreen.kt
@@ -1,6 +1,5 @@
 package com.example.moviedb.feature.profile.ui
 
-import ImagePicker
 import ImagePickerFactory
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -15,15 +14,12 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Person
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
