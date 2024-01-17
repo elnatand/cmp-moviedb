08634db5798a5b2a979d4a0f939ffc8459377811
Elna <ederech@digibank.co.il>
2024-01-17 07:58:45 +0200

    fix issues in iOS

--- core/database/build.gradle.kts
+++ core/database/build.gradle.kts
@@ -19,16 +19,35 @@ sqldelight {
 
 kotlin {
     sourceSets {
-        commonMain.dependencies {
-            implementation(libs.sqldelight.coroutines.extensions)
-            implementation(libs.koin.core)
-            implementation(projects.core.model)
+        val commonMain by getting {
+            dependencies {
+                implementation(libs.sqldelight.coroutines.extensions)
+                implementation(libs.koin.core)
+                implementation(projects.core.model)
+            }
         }
-        androidMain.dependencies {
-            implementation(libs.sqldelight.android.driver)
+        val androidMain by getting {
+            dependencies {
+                implementation(libs.sqldelight.android.driver)
+            }
         }
-        iosMain.dependencies {
-            implementation(libs.sqldelight.native.driver)
+        val iosX64Main by getting
+        val iosArm64Main by getting
+        val iosSimulatorArm64Main by getting
+        val iosMain by creating {
+            dependsOn(commonMain)
+            iosX64Main.dependsOn(this)
+            iosArm64Main.dependsOn(this)
+            iosSimulatorArm64Main.dependsOn(this)
+            dependencies {
+                implementation(libs.sqldelight.native.driver)
+
+                /**
+                 *  see why need this dependency
+                 *  https://github.com/cashapp/sqldelight/issues/4888#issuecomment-1846036472
+                 */
+                implementation("co.touchlab:stately-common:2.0.6")
+            }
         }
     }
 }
