7c50b58c2228ac405a8b56dd8d979bdb47e2c0c7
Elna <ederech@digibank.co.il>
2024-03-02 12:31:43 +0200

    update dependencies

--- features/profile/src/commonMain/kotlin/com/example/moviedb/feature/profile/ui/ProfileViewModel.kt
+++ features/profile/src/commonMain/kotlin/com/example/moviedb/feature/profile/ui/ProfileViewModel.kt
@@ -4,5 +4,5 @@ package com.example.moviedb.feature.profile.ui
 import moe.tlaster.precompose.viewmodel.ViewModel
 
 class ProfileViewModel : ViewModel() {
-
+    //implement here API call and logic
 }
