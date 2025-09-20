2ab0e68a5895cf4139d6e13b517f318823788e7e
Elnatan Derech <elnatand@gmail.com>
2025-09-20 16:59:26 +0300

    Remove KoinContext and LayoutDirectionWrapper (#16)

--- composeApp/src/commonMain/kotlin/com/example/moviedb/App.kt
+++ composeApp/src/commonMain/kotlin/com/example/moviedb/App.kt
@@ -4,26 +4,20 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import com.example.moviedb.navigation.RootNavGraph
-import com.example.moviedb.ui.LayoutDirectionWrapper
 import com.example.moviedb.ui.NavigationBar
-import org.koin.compose.KoinContext
 
 @Composable
 fun App() {
     val appState: AppState = rememberAppState()
-    KoinContext {
-        MaterialTheme {
-            LayoutDirectionWrapper {
-                Scaffold(
-                    bottomBar = {
-                        if (appState.shouldShowBottomBar()) {
-                            NavigationBar(appState = appState)
-                        }
-                    }
-                ) {
-                    RootNavGraph(appState.navController)
+    MaterialTheme {
+        Scaffold(
+            bottomBar = {
+                if (appState.shouldShowBottomBar()) {
+                    NavigationBar(appState = appState)
                 }
             }
+        ) {
+            RootNavGraph(appState.navController)
         }
     }
 }
