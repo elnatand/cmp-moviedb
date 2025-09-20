c23767f0bf0b7d42b93bbd381a7e2b26252f679b
elnatan <elnatand@gmail.com>
2025-09-20 16:09:18 +0300

    Remove KoinContext from App

--- composeApp/src/commonMain/kotlin/com/example/moviedb/App.kt
+++ composeApp/src/commonMain/kotlin/com/example/moviedb/App.kt
@@ -6,23 +6,20 @@ import androidx.compose.runtime.Composable
 import com.example.moviedb.navigation.RootNavGraph
 import com.example.moviedb.ui.LayoutDirectionWrapper
 import com.example.moviedb.ui.NavigationBar
-import org.koin.compose.KoinContext
 
 @Composable
 fun App() {
     val appState: AppState = rememberAppState()
-    KoinContext {
-        MaterialTheme {
-            LayoutDirectionWrapper {
-                Scaffold(
-                    bottomBar = {
-                        if (appState.shouldShowBottomBar()) {
-                            NavigationBar(appState = appState)
-                        }
+    MaterialTheme {
+        LayoutDirectionWrapper {
+            Scaffold(
+                bottomBar = {
+                    if (appState.shouldShowBottomBar()) {
+                        NavigationBar(appState = appState)
                     }
-                ) {
-                    RootNavGraph(appState.navController)
                 }
+            ) {
+                RootNavGraph(appState.navController)
             }
         }
     }
