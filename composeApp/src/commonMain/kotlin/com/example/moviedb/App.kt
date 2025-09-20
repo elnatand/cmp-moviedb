d253d1ad271b470bd1b6d78047f33a7c86fcf6d3
elnatan <elnatand@gmail.com>
2025-09-20 16:37:51 +0300

    remove LayoutDirectionWrapper

--- composeApp/src/commonMain/kotlin/com/example/moviedb/App.kt
+++ composeApp/src/commonMain/kotlin/com/example/moviedb/App.kt
@@ -4,23 +4,20 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import com.example.moviedb.navigation.RootNavGraph
-import com.example.moviedb.ui.LayoutDirectionWrapper
 import com.example.moviedb.ui.NavigationBar
 
 @Composable
 fun App() {
     val appState: AppState = rememberAppState()
     MaterialTheme {
-        LayoutDirectionWrapper {
-            Scaffold(
-                bottomBar = {
-                    if (appState.shouldShowBottomBar()) {
-                        NavigationBar(appState = appState)
-                    }
+        Scaffold(
+            bottomBar = {
+                if (appState.shouldShowBottomBar()) {
+                    NavigationBar(appState = appState)
                 }
-            ) {
-                RootNavGraph(appState.navController)
             }
+        ) {
+            RootNavGraph(appState.navController)
         }
     }
 }
