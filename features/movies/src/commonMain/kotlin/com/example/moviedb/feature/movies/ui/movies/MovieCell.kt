1f2fbc365cc13a093908a4704fd8547f9d39afd3
elnatan <elnatand@gmail.com>
2025-09-24 17:58:21 +0300

    add movie title to movie cell

--- features/movies/src/commonMain/kotlin/com/example/moviedb/feature/movies/ui/movies/MovieCell.kt
+++ features/movies/src/commonMain/kotlin/com/example/moviedb/feature/movies/ui/movies/MovieCell.kt
@@ -1,9 +1,14 @@
 package com.example.moviedb.feature.movies.ui.movies
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.example.moviedb.core.data.model.TMDB_IMAGE_URL
 import com.example.moviedb.core.model.Movie
@@ -16,8 +21,18 @@ fun MovieCell(
 ) {
     val imageUrl = "$TMDB_IMAGE_URL${movie.poster_path ?: ""}"
 
-    ImageLoader(
-        imageUrl = imageUrl,
-        modifier = Modifier.clickable { onClick(movie.id, movie.title) }.height(300.dp)
-    )
+    Column {
+        ImageLoader(
+            imageUrl = imageUrl,
+            modifier = Modifier.clickable { onClick(movie.id, movie.title) }.height(300.dp)
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Text(
+            textAlign = TextAlign.Left,
+            text = movie.title,
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
 }
