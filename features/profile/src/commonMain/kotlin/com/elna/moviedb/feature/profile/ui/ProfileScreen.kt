5064ff07f37c7021777d8c5133753daf18efa279
elna <elnatand@gmail.com>
2025-10-04 17:14:57 +0300

    Refactor: Remove unused Button import
    Removes an unused import of `androidx.compose.material3.Button` from `ProfileScreen.kt`.

--- features/profile/src/commonMain/kotlin/com/elna/moviedb/feature/profile/ui/ProfileScreen.kt
+++ features/profile/src/commonMain/kotlin/com/elna/moviedb/feature/profile/ui/ProfileScreen.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
