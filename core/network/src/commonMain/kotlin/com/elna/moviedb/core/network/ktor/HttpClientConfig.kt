202e41cfa695a1cd3442cecf7d8488cab7314ee5
elna <elnatand@gmail.com>
2025-10-25 11:07:05 +0300

    feat: Implement Ktor HTTP client logging
    This commit introduces logging for the Ktor HTTP client to aid in debugging network requests.
    
    Key changes:
    - **Dependency:** Added `ktor-client-logging` to the `core/network` module.
    - **Configuration:**
        - Configured the `Logging` plugin in the `HttpClient` to log the body of requests and responses (`LogLevel.BODY`).
        - Implemented a custom logger to print network logs to the console.
        - Enabled `prettyPrint` and `isLenient` in the `Json` configuration for more readable and flexible JSON parsing.

--- core/network/src/commonMain/kotlin/com/elna/moviedb/core/network/ktor/HttpClientConfig.kt
+++ core/network/src/commonMain/kotlin/com/elna/moviedb/core/network/ktor/HttpClientConfig.kt
@@ -4,13 +4,30 @@ import io.ktor.client.HttpClient
 import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 
 fun createHttpClient(httpClientEngine: HttpClientEngine) = HttpClient(httpClientEngine) {
     install(ContentNegotiation) {
-        json(Json { ignoreUnknownKeys = true })
+        json(Json {
+            prettyPrint = true
+            isLenient = true
+            ignoreUnknownKeys = true
+        })
     }
+
+    install(Logging) {
+        logger = object : Logger {
+            override fun log(message: String) {
+                println("http client log = $message")
+            }
+        }
+        level = LogLevel.BODY
+    }
+
     install(HttpTimeout) {
         requestTimeoutMillis = 15000
         connectTimeoutMillis = 15000
