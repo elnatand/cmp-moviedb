6071c3fd71e6c486d50331b8d1f88ee50254fd01
elna <elnatand@gmail.com>
2025-12-06 18:21:32 +0200

    chore: Remove iOS version update plugin
    This commit removes the `moviedb.iosVersionUpdate` plugin from the `composeApp/build.gradle.kts` file.

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.moviedb.composeMultiplatform)
-    alias(libs.plugins.moviedb.iosVersionUpdate)
 }
 
 kotlin {
