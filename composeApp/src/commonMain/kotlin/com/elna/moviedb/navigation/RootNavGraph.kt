34d1714bc6351d1ca309f3424ba65894656f7c67
elna <elnatand@gmail.com>
2025-12-19 09:16:13 +0200

    refactor: Remove unused imports in RootNavGraph
    This commit removes several unused import statements from `RootNavGraph.kt`. The deleted imports were related to specific navigation routes that are no longer directly referenced in this file.

--- composeApp/src/commonMain/kotlin/com/elna/moviedb/navigation/RootNavGraph.kt
+++ composeApp/src/commonMain/kotlin/com/elna/moviedb/navigation/RootNavGraph.kt
@@ -3,27 +3,17 @@ package com.elna.moviedb.navigation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.lifecycle.viewmodel.navigation3.rememberViewModelStoreNavEntryDecorator
-import androidx.navigation3.runtime.EntryProviderScope
 import androidx.navigation3.runtime.entryProvider
 import androidx.navigation3.runtime.rememberSaveableStateHolderNavEntryDecorator
 import androidx.navigation3.ui.NavDisplay
-import com.elna.moviedb.core.ui.navigation.MovieDetailsRoute
-import com.elna.moviedb.core.ui.navigation.MoviesRoute
-import com.elna.moviedb.core.ui.navigation.PersonDetailsRoute
-import com.elna.moviedb.core.ui.navigation.ProfileRoute
 import com.elna.moviedb.core.ui.navigation.Route
-import com.elna.moviedb.core.ui.navigation.SearchRoute
-import com.elna.moviedb.core.ui.navigation.TvShowDetailsRoute
-import com.elna.moviedb.core.ui.navigation.TvShowsRoute
 import com.elna.moviedb.feature.movies.navigation.movieDetailsEntry
 import com.elna.moviedb.feature.movies.navigation.moviesEntry
-import com.elna.moviedb.feature.movies.ui.movies.MoviesScreen
 import com.elna.moviedb.feature.person.navigation.personDetailsEntry
 import com.elna.moviedb.feature.profile.navigation.profileEntry
 import com.elna.moviedb.feature.search.navigation.searchEntry
 import com.elna.moviedb.feature.tvshows.navigation.tvShowDetailsEntry
 import com.elna.moviedb.feature.tvshows.navigation.tvShowsEntry
-
 
 @Composable
 fun RootNavGraph(
@@ -46,5 +36,3 @@ fun RootNavGraph(
         }
     )
 }
-
-
