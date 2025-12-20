014ec40d52ecfe570f82f9542a8f735f1ae6be8f
elna <elnatand@gmail.com>
2025-12-20 10:02:17 +0200

    chore: Remove unused import and update README
    This commit removes an unused import, `BoundsTransform`, from `MovieHeroSection.kt`. Additionally, it updates the project's README file to clarify that it is a multi-module Kotlin Multiplatform (KMP) application.

--- features/movies/src/commonMain/kotlin/com/elna/moviedb/feature/movies/ui/components/MovieHeroSection.kt
+++ features/movies/src/commonMain/kotlin/com/elna/moviedb/feature/movies/ui/components/MovieHeroSection.kt
@@ -1,7 +1,6 @@
 package com.elna.moviedb.feature.movies.ui.components
 
 import androidx.compose.animation.AnimatedVisibilityScope
-import androidx.compose.animation.BoundsTransform
 import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.animation.SharedTransitionScope
 import androidx.compose.animation.core.FastOutSlowInEasing
